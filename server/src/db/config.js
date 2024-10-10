21cf2eaa7ce327923c676ed26aa13075aa22f4bb
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2024-10-10 15:40:10 +0100

    database connection

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,12 @@
 // add your database connection here
+const { Sequelize } = require('sequelize');
+
+
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'database.sqlite')
+})
+
+module.exports = {
+    db
+}
